210e6596d49d182f964503b6d3eca90beebefdc0
Louis Teitelbaum <louis@teitelbaum.us>
2023-04-17 14:21:39 +0300

    update geom_worm example to show dodging

--- R/tests.R
+++ R/tests.R
@@ -1,7 +1,7 @@
 testdf <- data.frame(
-  x = c(1, 2, 3, 4, 1, 3, 4, 1, 1.5, 2.5, 4),
-  y = c(1, 1.9, 2, 2, 2, 1, 1, 1.5, 1, 1.7, 1.7),
-  group = rep(c("you", "me", "him"), times = c(4, 3, 4))
+  x = c(1, 2, 4, 1, 3, 4, 1, 1.5, 2.5, 4),
+  y = c(1, 2, 2, 2, 1, 1, 1.5, 1, 1.7, 1.7),
+  group = rep(c("you", "me", "him"), times = c(3, 3, 4))
 )
 
 test1 <- function(testdf) {
